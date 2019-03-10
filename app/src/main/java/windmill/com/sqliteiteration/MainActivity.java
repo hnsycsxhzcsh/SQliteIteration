8c6f9259e77ceda8f263863254ea12f636a92327
chen <151507670@qq.com>
2019-03-10 16:21:43 +0800

    新增示例

--- app/src/main/java/windmill/com/sqliteiteration/MainActivity.java
+++ app/src/main/java/windmill/com/sqliteiteration/MainActivity.java
@@ -20,6 +20,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mTvMessage = (TextView) findViewById(R.id.tv_message);
+        //也可使用query方法，我这里使用了rawQuery
+//        query(String table, String[] columns, String selection, String[] selectionArgs, String groupBy, String having, String orderBy, String limit)  查询语句
+//        table为表名
+//        columns想要显示的字段
+//        selection条件
+//        selectionArgs 中为selection的值
+//        groupBy 分组
+//        having  分组条件
+//        orderBy 排序
+//        limit 分页
         //TODO 因为数据库操作是耗时操作，以下的这些操作需放在异步中进行操作，我这里为了简便没有放异步中
 
         findViewById(R.id.bt_search).setOnClickListener(new View.OnClickListener() {
@@ -45,6 +55,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+//        insert(String table, String nullColumnHack, ContentValues values) 插入语句，
+//        table表名
+//        values为ContentValues对象！该对象存储方式为Key-value，Key表示表中的字段名称，value表示该字段对应的值
+//        nullColumnHack表示强行向表中插入null，前提是第三个参数（values）为空或者不包含任何Key-value时生效
         findViewById(R.id.bt_insert1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -69,11 +83,15 @@ public class MainActivity extends AppCompatActivity {
                 cv.put("body", "body" + count);
                 cv.put("pic", "pic" + count);
                 count++;
-                sqLiteDatabase.insert(MySQLiteOpenHelper.TABLE_NAME, null, cv);
+                long insert = sqLiteDatabase.insert(MySQLiteOpenHelper.TABLE_NAME, null, cv);
                 instance.closeDatabase();
             }
         });
 
+//        update(String table, ContentValues values, String whereClause, String[] whereArgs)  更新语句
+//        table为表名
+//        values为ContentValues对象！该对象存储方式为Key-value，Key表示表中的字段名称
+//        value表示该字段对应的值，第三个参数为条件例如：_ID = ? ,第四个参数为第三个参数的值！
         findViewById(R.id.bt_update).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -87,6 +105,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+//        delete(String table, String whereClause, String[] whereArgs)  删除数据
+//        table表名
+//        whereClause为条件例如：_ID = ?
+//        whereArgs为whereClause的值！
         findViewById(R.id.bt_delete).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
